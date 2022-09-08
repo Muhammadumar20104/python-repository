22099fb7fbe95fe77d4947bafd1d6fe0086580d1
Muhammadumar20104 <raoumar20104@gmail.com>
2022-09-08 10:31:14 -0700

    condition if else

---
+++ tut5.py
@@ -0,0 +1,15 @@
+age=27
+if age<26:
+    print(age)
+else:
+    print("out of age")
+    
+name=str(input("Enter the name "))
+if name=="lucky":
+    print("lucky")
+elif name=="ducky":
+    print("ducky")
+elif name=="bucky":
+    print("bucky")
+else:
+    print("incorrect")
